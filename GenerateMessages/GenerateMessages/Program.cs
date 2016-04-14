c03a2c8c38dbb109c0745202e86f2de2e836a27c
Jef King <jefkin@microsoft.com>
2016-04-14 11:38:43 -0700

    pulling connection

--- GenerateMessages/GenerateMessages/Program.cs
+++ GenerateMessages/GenerateMessages/Program.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GenerateMessages
+﻿namespace GenerateMessages
 {
+    using System.Configuration;
+
     class Program
     {
         static void Main(string[] args)
         {
+            var connection = ConfigurationManager.AppSettings["Microsoft.ServiceBus.ConnectionString"];
         }
     }
 }
