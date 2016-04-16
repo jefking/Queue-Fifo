6bfea57764e7c7b2bd6e8255d3524b221f01dc5c
Jef King <jef@agilebusinesscloud.com>
2016-04-16 08:49:44 -0700

    more code

--- GenerateMessages/PullMessages/Program.cs
+++ GenerateMessages/PullMessages/Program.cs
@@ -1,9 +1,12 @@
 ﻿namespace PullMessages
 {
+    using System;
     using System.Configuration;
+    using System.Diagnostics;
     using System.Threading.Tasks;
     using King.Service.ServiceBus;
-
+    using King.Service.ServiceBus.Wrappers;
+    using Microsoft.ServiceBus.Messaging;
     class Program
     {
         static void Main(string[] args)
@@ -20,6 +23,24 @@
 
             var subscriber = new BusTopicSubscriber(topicName, connection, subscriptionName, filter);
             await subscriber.CreateIfNotExists();
+
+            var tc = TopicClient.CreateFromConnectionString(connection, topicName);
+            var s = new BusEvents<Sample>(new BusTopicClient(tc), new Handler());
+        }
+
+        public class Handler : IBusEventHandler<Sample>
+        {
+            public void OnError(string action, Exception ex)
+            {
+                Trace.TraceError("Action '{0}' caused: {1}", action, ex);
+            }
+
+            public Task<bool> Process(Sample data)
+            {
+                Trace.TraceInformation(data.ToString());
+
+                return true;
+            }
         }
     }
 }
