980deba5081d4c0ca3dd881db91d81162fabc8ff
Jef King <jef@agilebusinesscloud.com>
2016-05-12 11:54:39 -0700

    Compiling

--- ReliableActor/Device/Device.cs
+++ ReliableActor/Device/Device.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using Microsoft.ServiceFabric.Actors;
-using Microsoft.ServiceFabric.Actors.Runtime;
-using Microsoft.ServiceFabric.Actors.Client;
+﻿using System.Threading.Tasks;
 using Device.Interfaces;
+using Microsoft.ServiceFabric.Actors.Runtime;
 
 namespace Device
 {
