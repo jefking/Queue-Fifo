5075d747efabd94eaaef1c2ec5b020d376eb6077
Jef King <jefkin@microsoft.com>
2016-05-16 19:56:38 -0700

    more betterz

---
+++ ReliableActor/DataEntry/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using Actors.Interfaces;
+using Microsoft.ServiceFabric.Actors;
+using Microsoft.ServiceFabric.Actors.Client;
+using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace DataEntry.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values 
+        public async Task<bool> Get(Guid id)
+        {
+            var actorId = ActorId.CreateRandom();
+            var device = ActorProxy.Create<IDevice>(actorId, new Uri("fabric:/Devices/DeviceActorService"));
+            return await device.Process(id);
+        }
+    }
+}
